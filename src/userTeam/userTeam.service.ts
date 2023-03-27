bef80577ca0af68045a8c1a54847ae010f6eccd0
mohamed-tarek-fouad <mt1348@fayoum.edu.eg>
2023-03-27 02:14:29 +0200

    fix

--- src/userTeam/userTeam.service.ts
+++ src/userTeam/userTeam.service.ts
@@ -131,7 +131,7 @@ export class UserTeamService {
       for (let i = 0; i < team.length; i++) {
         if (map[team[i].teamId]) {
           map[team[i].teamId] += 1;
-          if (map[team[i].teamId] === 2) {
+          if (map[team[i].teamId] === 3) {
             throw new HttpException(
               "you can have only 2 players from the same team",
               HttpStatus.BAD_REQUEST,
